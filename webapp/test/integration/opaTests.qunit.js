ac34b445d18e4d31f2f3e1d5ca0c0dbd5e2ddc91
Marcelo Galimberti <galimbertimarcelo@gmail.com>
2025-03-29 13:41:32 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/bootcamp/sapui5/tpfinalfiori/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/bootcamp/sapui5/tpfinalfiori/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
